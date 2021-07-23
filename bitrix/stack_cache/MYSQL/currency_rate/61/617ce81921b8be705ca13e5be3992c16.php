eb7deec27ca8adcf36efaadb7a72ef79b13793c8
klimov-rv <klimov@perco.ru>
2021-07-23 12:59:57 +0300

    some fixes 21-23 july

--- bitrix/stack_cache/MYSQL/currency_rate/61/617ce81921b8be705ca13e5be3992c16.php
+++
@@ -1,7 +0,0 @@
-<?
-if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001626332610';
-$dateexpire = '001626343410';
-$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";a:1:{s:22:"C_R_2021-07-16_EUR_RUB";a:4:{s:11:"curFromRate";d:87.3917;s:14:"curFromRateCnt";i:1;s:9:"curToRate";d:1;s:12:"curToRateCnt";i:1;}}}';
-return true;
-?>
