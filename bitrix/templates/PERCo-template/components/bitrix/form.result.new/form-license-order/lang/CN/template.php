e3f3c2f632a3f6d0b6da3f4efbb5fa4fb399e57c
klimov-rv <klimov@perco.ru>
2021-07-26 16:29:23 +0300

    new .gitinore

---
+++ bitrix/templates/PERCo-template/components/bitrix/form.result.new/form-license-order/lang/CN/template.php
@@ -0,0 +1,10 @@
+<?
+$MESS ['FORM_REQUIRED_FIELDS'] = "±ШРиЧЦ¶ОЈЁРЗєЕ±кјЗЈ©";
+$MESS ['FORM_APPLY'] = "У¦УГ";
+$MESS ['FORM_ADD'] = "РВЅЁ";
+$MESS ['FORM_ACCESS_DENIED'] = "WEB-±нµҐ·ГОКЅыЦ№ЎЈ";
+$MESS ['FORM_DATA_SAVED1'] = "Р»Р»ДгЎЈ ДгµДЙкЗл±н#";
+$MESS ['FORM_DATA_SAVED2'] = "ТСКХµЅ";
+$MESS ['FORM_MODULE_NOT_INSTALLED'] = "Web-form module is not installed.";
+$MESS ['FORM_NOT_FOUND'] = "Web-form is not found.";
+?>
---
+++ bitrix/templates/PERCo-template/components/bitrix/form.result.new/form-license-order/lang/vn/template.php
@@ -0,0 +1,19 @@
+<?
+/**
+ * This is the translation of VietNamese language files for BitrixSoft
+ * @version 1.0
+ * @copyright V&V IDS, JSC 2007
+ * @website: www.bitrixsoft.vn, vivicorp.com
+ * @email: contact@vivicorp.com
+ * @phone: (+84)46416724	(+84)42910711
+ */
+ 
+$MESS ['FORM_REQUIRED_FIELDS'] = "CГЎc trЖ°б»ќng bбєЇt buб»™c";
+$MESS ['FORM_APPLY'] = "ГЃp dб»Ґng";
+$MESS ['FORM_ADD'] = "ThГЄm";
+$MESS ['FORM_ACCESS_DENIED'] = "Mбє«u Web bб»‹ cбєҐm truy cбє­p.";
+$MESS ['FORM_DATA_SAVED1'] = "CбєЈm ЖЎn. б»Ёng dб»Ґng biб»ѓu mбє«u #";
+$MESS ['FORM_DATA_SAVED2'] = " Д‘ГЈ nhбє­n.";
+$MESS ['FORM_MODULE_NOT_INSTALLED'] = "MГґД‘un biб»ѓu mбє«u web chЖ°a Д‘Ж°б»Јc cГ i Д‘бє·t.";
+$MESS ['FORM_NOT_FOUND'] = "Biб»ѓu mбє«u web chЖ°a tГ¬m thбєҐy.";
+?>
